c2f09f0c04b26ccb8b9f32b75c6b198ef0ac0422
ShraddhaChetule123 <85213968+ShraddhaChetule123@users.noreply.github.com>
2022-04-12 23:58:41 +0530

    Project created

--- db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-module.exports = mongoose
-  .connect("mongodb+srv://inzam918:inzam918@cluster0.ew83k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-  })
-  .then(() => console.log("connected to database"))
-  .catch(err => console.error("could not connect to database", err));
